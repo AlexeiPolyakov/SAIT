879d5a1b258699cb990e1269a1bdd4c1f753ad29
AlexeiPolyakov <alexei.polyakov444@gmail.com>
2024-05-27 12:14:53 +0300

    Добавьте файлы проекта.

---
+++ include/header.php
@@ -0,0 +1,47 @@
+<header class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <h1>
+                        <a href="/"><img src="images/logo_1.png"></a>
+                    </h1>
+                </div>
+                <nav class="col-8">
+                    <ul>
+                        <li><a href="#">Главная</a> </li>
+
+                        <li>
+                            <a href="#">Проекты</a>
+                            <ul>
+                                <li><a href="#">Дома из бруса</a> </li>
+                                <li><a href="#">Бани из бруса</a> </li>
+                            </ul>
+                        </li>
+
+                        <li><a href="#">Галерея</a> </li>
+
+                        <li>
+                            <a href="#">Услуги</a>
+                            <ul>
+                                <li><a href="#">Возведение фундаментов</a> </li>
+                                <li><a href="#">Строительство домов из бруса</a> </li>
+                                <li><a href="#">Внутренняя отделка</a> </li>
+                                <li><a href="#">Внешняя отделка</a> </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="#">О нас</a>
+                            <ul>
+                                <li><a href="#">Отзывы</a> </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="#">Контакты</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
